edcb256fb941a26e91b23a24445339e5f6aafe49
Ge Zhang <gsarazh@gmail.com>
2021-02-15 18:07:23 +0800

    insert cell to wrapper

--- src/branch-btn.ts
+++ src/branch-btn.ts
@@ -60,19 +60,25 @@ function subdivideCell(panel: NotebookPanel){
   var activeCellIndex: number = getActiveCellIndex(panel, isInWrapper);
 
   if (isInWrapper){
-    console.log('TODO');
+    console.log('active cell is in a wrapper');
 
   }else{
+    console.log('active cell not in wrapper. Create a new wrapper for it');
     var newWrapper = document.createElement('div');
     newWrapper.classList.add('wrapper');
     newWrapper.appendChild(activeCell.node);
 
     panel.content.node.insertBefore(newWrapper, panel.content.node.children[activeCellIndex])
-
-    console.log('TODO11！！！');
-
-
+    // console.log('TODO11！！！');
   }
 
+  console.log('create a new cell and insert it to the wrapper');
+  var wrapper = panel.content.node.children[activeCellIndex];
+  var newCell = panel.model.contentFactory.createCell('code', {});
+  panel.model.insertCell(activeCellIndex+1, newCell);
+  // panel.model.ycells.insert(activeCellIndex+1, newCell);
+  wrapper.appendChild(panel.content.node.children[activeCellIndex+1]);
 
+
+  
 }
